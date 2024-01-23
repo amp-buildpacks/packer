ff73f3ffde0fab71633c69b7afc40662be78689a
lispking <lispking@qq.com>
2024-01-23 22:39:40 +0800

    issue(#10): Add PlanEntryResolver handle for buildpack

--- assets/templates/buildpack/go.mod
+++ assets/templates/buildpack/go.mod
@@ -1,23 +1,23 @@
 module github.com/amp-buildpacks/{{ packer_name }}
 
-go 1.20
+go 1.21
 
 require (
 	github.com/buildpacks/libcnb v1.30.1
-	github.com/paketo-buildpacks/libpak v1.68.0
+	github.com/paketo-buildpacks/libpak v1.68.1
 )
 
 require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
 	github.com/Masterminds/semver/v3 v3.2.1 // indirect
-	github.com/creack/pty v1.1.20 // indirect
+	github.com/creack/pty v1.1.21 // indirect
 	github.com/h2non/filetype v1.1.3 // indirect
 	github.com/heroku/color v0.0.6 // indirect
 	github.com/imdario/mergo v0.3.16 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/hashstructure/v2 v2.0.2 // indirect
-	github.com/onsi/gomega v1.30.0 // indirect
+	github.com/onsi/gomega v1.31.1 // indirect
 	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
-	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 )
--- assets/templates/buildpack/packer/packer.go
+++ assets/templates/buildpack/packer/packer.go
@@ -29,58 +29,66 @@ import (
 	"github.com/paketo-buildpacks/libpak/sherpa"
 )
 
-type CustomLayer struct {
+type {{ packer_name | capitalize }} struct {
 	LayerContributor libpak.DependencyLayerContributor
 	Logger           bard.Logger
 	Executor         effect.Executor
 }
 
-func NewCustomLayer(dependency libpak.BuildpackDependency, cache libpak.DependencyCache) CustomLayer {
+func New{{ packer_name | capitalize }}(dependency libpak.BuildpackDependency, cache libpak.DependencyCache) {{ packer_name | capitalize }} {
 	contributor := libpak.NewDependencyLayerContributor(dependency, cache, libcnb.LayerTypes{
 		Build:  true,
 		Cache:  true,
 		Launch: true,
 	})
-	return CustomLayer{
+	return {{ packer_name | capitalize }}{
 		LayerContributor: contributor,
 		Executor:         effect.NewExecutor(),
 	}
 }
 
-func (c CustomLayer) Contribute(layer libcnb.Layer) (libcnb.Layer, error) {
-	c.LayerContributor.Logger = c.Logger
-	return c.LayerContributor.Contribute(layer, func(artifact *os.File) (libcnb.Layer, error) {
+func (r {{ packer_name | capitalize }}) Contribute(layer libcnb.Layer) (libcnb.Layer, error) {
+	r.LayerContributor.Logger = r.Logger
+	return r.LayerContributor.Contribute(layer, func(artifact *os.File) (libcnb.Layer, error) {
 		// TODO: update below codes to your buildpack
 		bin := filepath.Join(layer.Path, "bin")
 
-		// TODO: May be use copy instead of it.
-		c.Logger.Bodyf("Expanding %s to %s", artifact.Name(), bin)
+		// TODO: May be use copy instead of it or update Extract Path or stripComponents=1
+		r.Logger.Bodyf("Expanding %s to %s", artifact.Name(), bin)
 		if err := crush.Extract(artifact, layer.Path, 1); err != nil {
 			return libcnb.Layer{}, fmt.Errorf("unable to expand %s\n%w", artifact.Name(), err)
 		}
 
+		// Must be set to executable
+		// TODO: May be update bin-name
+		file := filepath.Join(bin, "{{ packer_name }}")
+		r.Logger.Bodyf("Setting %s as executable", file)
+		if err := os.Chmod(file, 0755); err != nil {
+			return libcnb.Layer{}, fmt.Errorf("unable to chmod %s\n%w", file, err)
+		}
+
 		// Must be set to PATH
-		c.Logger.Bodyf("Setting %s in PATH", bin)
+		r.Logger.Bodyf("Setting %s in PATH", bin)
 		if err := os.Setenv("PATH", sherpa.AppendToEnvVar("PATH", ":", bin)); err != nil {
 			return libcnb.Layer{}, fmt.Errorf("unable to set $PATH\n%w", err)
 		}
 
 		// get {{ packer_name }} version
-		buf, err := c.Execute("{{ packer_name }}", []string{"--version"})
+		buf, err := r.Execute("{{ packer_name }}", []string{"--version"})
 		if err != nil {
 			return libcnb.Layer{}, fmt.Errorf("unable to get {{ packer_name }} version\n%w", err)
 		}
 		version := strings.TrimSpace(buf.String())
-		c.Logger.Bodyf("Checking {{ packer_name }} version: %s", version)
+		r.Logger.Bodyf("Checking {{ packer_name }} version: %s", version)
 
 		// TODO: May be need more codes...
 		return layer, nil
 	})
 }
 
-func (c CustomLayer) Execute(command string, args []string) (*bytes.Buffer, error) {
+func (r {{ packer_name | capitalize }}) Execute(command string, args []string) (*bytes.Buffer, error) {
 	buf := &bytes.Buffer{}
-	if err := c.Executor.Execute(effect.Execution{
+	if err := r.Executor.Execute(effect.Execution{
 		Command: command,
 		Args:    args,
 		Stdout:  buf,
@@ -91,6 +99,23 @@ func (c CustomLayer) Execute(command string, args []string) (*bytes.Buffer, erro
 	return buf, nil
 }
 
-func (c CustomLayer) Name() string {
-	return c.LayerContributor.LayerName()
+
+func (r {{ packer_name | capitalize }}) BuildProcessTypes(cr libpak.ConfigurationResolver, app libcnb.Application) ([]libcnb.Process, error) {
+	processes := []libcnb.Process{}
+
+	enableDeploy := cr.ResolveBool("BP_ENABLE_{{ packer_name | upper }}_PROCESS")
+	if enableDeploy {
+		// TODO: Update run command and args
+		processes = append(processes, libcnb.Process{
+			Type:    "web",
+			Command: "<run-command>",
+			Arguments: []string{"<command-args>"},
+			Default: true,
+		})
+	}
+	return processes, nil
+}
+
+func (r {{ packer_name | capitalize }}) Name() string {
+	return r.LayerContributor.LayerName()
 }
